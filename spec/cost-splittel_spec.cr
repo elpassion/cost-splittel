ff27b8d61a140677a3bd7579a09730fe82c1c867
Arkadiusz Oleksy <oleksy.arkadiusz@gmail.com>
2019-01-10 10:47:52 +0100

    Initial commit

---
+++ spec/cost-splittel_spec.cr
@@ -0,0 +1,9 @@
+require "./spec_helper"
+
+describe Cost::Splittel do
+  # TODO: Write tests
+
+  it "works" do
+    false.should eq(true)
+  end
+end
---
+++ src/cost-splittel.cr
@@ -0,0 +1,6 @@
+# TODO: Write documentation for `Cost::Splittel`
+module Cost::Splittel
+  VERSION = "0.1.0"
+
+  # TODO: Put your code here
+end
